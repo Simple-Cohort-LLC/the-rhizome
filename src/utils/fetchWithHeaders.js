16c9ee5784d3f7ccde8636853348bc32ad1b02c7
Sean Robenalt <srob0722@gmail.com>
2025-01-28 14:04:54 -0500

    json

--- src/utils/fetchWithHeaders.js
+++ src/utils/fetchWithHeaders.js
@@ -1,6 +1,7 @@
 export const fetchWithHeaders = async (url, options = {}) => {
   const defaultHeaders = {
     "x-api-key": process.env.EXPO_PUBLIC_API_KEY,
+    "content-type": "application/json",
   };
   const mergedOptions = {
     ...options,
